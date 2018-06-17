6cfcdec157499f62f138544bdead91b67f7ae12c
ChristopheSnacken <c.snacken@gmail.com>
2018-06-17 17:24:44 +0200

    disable button after click - make extra component for alphabet yet to come

--- src/components/Board.js
+++ src/components/Board.js
@@ -6,11 +6,15 @@ import {randomWord,showGuess,wrongGuessCount,wrongGuessLimit,isWinner} from '../
 import './Board.css'
 
 
+
 class Board extends PureComponent {
 
 
 
-
+alphabetbutton() {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  return alphabet.map( letter => <button disabled = {this.props.guesses.indexOf(letter) >= 0} className="guesses" onClick={() => this.props.makeGuess(letter)}>{letter}</button>)
+}
 
   componentWillMount() {
     this.props.newGame(randomWord())
@@ -50,47 +54,9 @@ class Board extends PureComponent {
 
         {/* Ugly but works, have to give some attention to my kids birthday sorry!*/}
         <div className="wrapper">
-          <div>
-            <button className="guesses" onClick={() => this.props.makeGuess('a')}>a</button>
-            <button className="guesses" onClick={() => this.props.makeGuess('b')}>b</button>
-            <button className="guesses" onClick={() => this.props.makeGuess('c')}>c</button>
-            <button className="guesses" onClick={() => this.props.makeGuess('d')}>d</button>
-            <button className="guesses" onClick={() => this.props.makeGuess('e')}>e</button>
-            <button className="guesses" onClick={() => this.props.makeGuess('f')}>f</button>
-          </div>
-          <div>
-            <button className="guesses" onClick={() => this.props.makeGuess('g')}>g</button>
-            <button className="guesses" onClick={() => this.props.makeGuess('h')}>h</button>
-            <button className="guesses" onClick={() => this.props.makeGuess('i')}>i</button>
-            <button className="guesses" onClick={() => this.props.makeGuess('j')}>j</button>
-            <button className="guesses" onClick={() => this.props.makeGuess('k')}>k</button>
-            <button className="guesses" onClick={() => this.props.makeGuess('l')}>l</button>
-          </div>
-          <div>
-            <button className="guesses" onClick={() => this.props.makeGuess('m')}>m</button>
-            <button className="guesses" onClick={() => this.props.makeGuess('n')}>n</button>
-            <button className="guesses" onClick={() => this.props.makeGuess('o')}>o</button>
-            <button className="guesses" onClick={() => this.props.makeGuess('p')}>p</button>
-            <button className="guesses" onClick={() => this.props.makeGuess('q')}>q</button>
-            <button className="guesses" onClick={() => this.props.makeGuess('r')}>r</button>
-          </div>
-          <div>
-            <button className="guesses" onClick={() => this.props.makeGuess('r')}>s</button>
-            <button className="guesses" onClick={() => this.props.makeGuess('t')}>t</button>
-            <button className="guesses" onClick={() => this.props.makeGuess('u')}>u</button>
-            <button className="guesses" onClick={() => this.props.makeGuess('v')}>v</button>
-            <button className="guesses" onClick={() => this.props.makeGuess('w')}>w</button>
-            <button className="guesses" onClick={() => this.props.makeGuess('x')}>x</button>
-          </div>
-          <div>
-            <button className="guesses" onClick={() => this.props.makeGuess('y')}>y</button>
-            <button className="guesses" onClick={() => this.props.makeGuess('z')}>z</button>
-          </div>
+
+          {this.alphabetbutton()}
         </div>
-
-
-
-
 
       </div>
 
